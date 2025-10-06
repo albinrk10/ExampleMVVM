4027fa4b372cbdcf56c563d10b41f898bf163a6a
albin <albinrk@gmail.com>
2025-10-06 15:55:01 -0500

    Initial commit

---
+++ app/src/main/java/com/albin/examplemvvm/model/QuoteModel.kt
@@ -0,0 +1,8 @@
+package com.albin.examplemvvm.model
+
+
+
+data class QuoteModel(
+    val quote: String,
+    val author: String
+)
---
+++ app/src/main/java/com/albin/examplemvvm/view/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.albin.examplemvvm.view
+
+import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import com.albin.examplemvvm.databinding.ActivityMainBinding
+import com.albin.examplemvvm.viewmodel.QuoteViewModel
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var  binding: ActivityMainBinding
+
+    private val quoteViewModel: QuoteViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        quoteViewModel.quoteModel.observe(this, Observer { currentQuote ->
+            binding.tvQuote.text= currentQuote.quote
+            binding.tvAuthor.text=currentQuote.author
+        })
+        binding.viewContainer.setOnClickListener { quoteViewModel.randomQuote() }
+    }
+}
